2b77d53f043d4c22c7260cf74cd892dfcd810af1
eabra <emab21@student.bth.se>
2022-09-25 18:32:01 +0200

    fix code styling with eslint

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 
 import { BrowserRouter as Router, Route, Link, Routes}
-        from "react-router-dom";
+    from "react-router-dom";
 
-import Home from "./components/Welcome"
-import Editor from "./components/Editor"
-import Page2 from "./components/page2"
-import Page3 from "./components/page3"
+import Home from "./components/Welcome";
+import Editor from "./components/Editor";
+import Page2 from "./components/page2";
+import Page3 from "./components/page3";
 
 import docsModel from './models/docs';
 import {useState, useEffect} from 'react';
-import "./App.css"
+import "./App.css";
 
 function App() {
     const [docs, setDocs] = useState([]);
@@ -29,26 +29,26 @@ function App() {
     }, [currentDoc]);
 
     return (
-    <div className="App">
-        <header className="header">
-            <Router>
-                <div className="list">
-                    <ul className="ul-nav">
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="editor">Editor</Link></li>
-                        <li><Link to="page2">To be...</Link></li>
-                        <li><Link to="page3">To be...</Link></li>
-                    </ul>
-                </div>
-                <Routes>
-                    <Route exact path="/" element={<Home />} />
-                    <Route exact path="editor" element={<Editor docs={docs} setAlldocs={fetchDocs} setCurrentDoc={setCurrentDoc} currentDoc={currentDoc} />} />
-                    <Route exact path="page2" element={<Page2 />} />
-                    <Route exact path="page3" element={<Page3 />} />
-                </Routes>
-            </Router>
-        </header>
-    </div>
+        <div className="App">
+            <header className="header">
+                <Router>
+                    <div className="list">
+                        <ul className="ul-nav">
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="editor">Editor</Link></li>
+                            <li><Link to="page2">To be...</Link></li>
+                            <li><Link to="page3">To be...</Link></li>
+                        </ul>
+                    </div>
+                    <Routes>
+                        <Route exact path="/" element={<Home />} />
+                        <Route exact path="editor" element={<Editor docs={docs} setAlldocs={fetchDocs} setCurrentDoc={setCurrentDoc} currentDoc={currentDoc} />} />
+                        <Route exact path="page2" element={<Page2 />} />
+                        <Route exact path="page3" element={<Page3 />} />
+                    </Routes>
+                </Router>
+            </header>
+        </div>
     );
 }
 

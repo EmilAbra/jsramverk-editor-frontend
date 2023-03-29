285e25395e131e4158bb421ede5651302cfa2087
eabra <emab21@student.bth.se>
2023-03-29 14:48:58 +0200

    rename to test function

--- src/components/DocSelect.test.jsx
+++ src/components/DocSelect.test.jsx
@@ -1,18 +1,20 @@
 import { render, screen } from "@testing-library/react";
-import SelectAllDocs from "./DocsAll";
+import SelectAllDocs from "./DocSelect";
 
 const allDocs = [
   {
     name: "doc1",
     content: "Lorem ipsum",
+    codeMode: false
   },
   {
     name: "doc2",
     content: "Lorem ipsum dolor sit amet",
+    codeMode: false
   },
 ];
 
-it("renders all doc names in select element", () => {
+test("renders all doc names in select element", () => {
   render(<SelectAllDocs docs={allDocs} />);
   const docElements = screen.getAllByRole("option", { value: /doc/ });
   expect(docElements).toHaveLength(3);
